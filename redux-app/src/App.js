b60c95c3bcb724aa45d13e352720ee6b34275368
saurabh <saurabhgirepunje97@gmail.com>
2023-02-25 19:38:45 +0530

    3rd

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-          Learn Redux app - write it
+          Learn Redux app - write 
       </header>
     </div>
   );
