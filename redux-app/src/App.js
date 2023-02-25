593fe4ef743f6b8fa95ce4924333b790614434b0
saurabh <saurabhgirepunje97@gmail.com>
2023-02-25 19:30:29 +0530

    2nd commit

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-          Learn Redux app
+          Learn Redux app - write it
       </header>
     </div>
   );
